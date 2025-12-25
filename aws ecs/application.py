a705c3fc0782ff674ed1ca791b5553b76877600d
iamrubayet <rubayetaivrda@gmail.com>
2025-12-26 02:07:46 +0600

    chore : added initial files

---
+++ aws ecs/application.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+port = int(os.environ.get('PORT', 5000))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=port, debug=True)
